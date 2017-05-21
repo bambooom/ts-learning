c5825ab4f26a794247dea3bcdd7e05f3b1b848c9
zhuzi <nmz89825@gmail.com>
2017-05-21 16:56:04 +0800

    <fix> HoldingPos structure and style

--- src/components/HoldingPos.tsx
+++ src/components/HoldingPos.tsx
@@ -25,20 +25,64 @@ export interface Props {
 export default class HoldingPos extends React.Component<Props, object> {
     renderCapitalSection() {
         return (
-            <div className="capital card-body">
-                <div className="columns">
-                    <div className="column col-6">
-                        总资产: 103125
+            <div className="card capital">
+                <div className="card-header">
+                    <div className="card-title">资产</div>
+                </div>
+                <div className="card-body">
+                    <div className="columns">
+                        <div className="column col-6">
+                            总资产: 103125
                         </div>
-                    <div className="column col-6">
-                        证券市值: 0
+                        <div className="column col-6">
+                            证券市值: 0
                         </div>
-                    <div className="column col-6">
-                        现金: 103125
+                        <div className="column col-6">
+                            现金: 103125
                         </div>
-                    <div className="column col-6">
-                        冻结金额: 0
+                        <div className="column col-6">
+                            冻结金额: 0
                         </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+    renderSecuritySection(security: Holding[]) {
+        const rows = security.map(stock => (
+            <tr key={Math.random()}>
+                <td>{stock.code}</td>
+                <td>{stock.name}</td>
+                <td>{stock.quantity}</td>
+                <td>{stock.avgCostPrice}</td>
+                <td>{stock.ProfitAndLossNum}</td>
+                <td>{stock.ProfitAndLossPct}</td>
+                <td>{stock.marketValue}</td>
+            </tr>
+        ));
+        return (
+            <div className="card security">
+                <div className="card-header">
+                    <div className="card-title">持仓</div>
+                </div>
+                <div className="security-table">
+                    <table className="table table-striped table-hover">
+                        <thead>
+                            <tr>
+                                <th>代号</th>
+                                <th>名称</th>
+                                <th>持有数量</th>
+                                <th>成本价</th>
+                                <th>盈亏金额</th>
+                                <th>盈亏比例</th>
+                                <th>市值</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {rows}
+                        </tbody>
+                    </table>
                 </div>
             </div>
         );
@@ -57,41 +101,10 @@ export default class HoldingPos extends React.Component<Props, object> {
                 marketValue: 46360,
             }
         ];
-        const rows = security.map(stock => (
-            <tr key={Math.random()}>
-                <td>{stock.code}</td>
-                <td>{stock.name}</td>
-                <td>{stock.quantity}</td>
-                <td>{stock.avgCostPrice}</td>
-                <td>{stock.ProfitAndLossNum}</td>
-                <td>{stock.ProfitAndLossPct}</td>
-                <td>{stock.marketValue}</td>
-            </tr>
-        ));
         return (
             <div className="card holdings">
-                <div className="card-header">
-                    <div className="card-title">资产及持仓</div>
-                </div>
                 {this.renderCapitalSection()}
-                <div className="security">
-                    <table className="table table-striped table-hover">
-                        <thead>
-                            <tr>
-                                <th>代号</th>
-                                <th>名称</th>
-                                <th>持有数量</th>
-                                <th>成本价</th>
-                                <th>盈亏金额</th>
-                                <th>盈亏比例</th>
-                                <th>市值</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {rows}
-                        </tbody>
-                    </table>
-                </div>
+                {this.renderSecuritySection(security)}
             </div>
         );
     }
